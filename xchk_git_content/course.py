674d13c5494bd78160d348af922a70fedee8eaa7
Vincent Nys <vincentnys@gmail.com>
2020-07-25 09:56:47 +0200

    Begin cursus Git.

--- xchk_git_content/course.py
+++ xchk_git_content/course.py
@@ -1,4 +1,5 @@
 from xchk import contentviews as basecv
+from xchk.courses import Course
 from .contentviews import *
 
-course = []
+course = Course('git','Git',[(WhatIsGitView,[basecv.ImpossibleNodeView])],"git@github.com:v-nys/git-modeloplossingen.git")
