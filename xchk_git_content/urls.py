af84a1c12df034e2b734e612e5964fc6a3910c87
Vincent Nys <vincentnys@gmail.com>
2020-07-25 10:01:46 +0200

    URL eerste content.

--- xchk_git_content/urls.py
+++ xchk_git_content/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import contentviews
 
 urlpatterns = [
+    path('what_is_git_1', contentviews.WhatIsGitView.as_view(), name=f'{contentviews.WhatIsGitView.uid}_view'),
 ]
