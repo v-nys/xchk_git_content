7d0050d8ba31791cf756d71ce949938b0ff21b36
Vincent Nys <vincentnys@gmail.com>
2020-07-26 08:57:07 +0200

    Nog boilerplate die moest worden aangepast.

--- xchk_git_content/urls.py
+++ xchk_git_content/urls.py
@@ -4,5 +4,5 @@ from . import contentviews
 
 urlpatterns = [
     path('what_is_git_1', contentviews.WhatIsGitView.as_view(), name=f'{contentviews.WhatIsGitView.uid}_view'),
-    path('start_git_init_1', contentviews.GitInitView.as_view(), name=f'{contentviews.WhatIsGitView.uid}_view'),
+    path('start_git_init_1', contentviews.GitInitView.as_view(), name=f'{contentviews.GitInitView.uid}_view'),
 ]
