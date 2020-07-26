b546c9a0f0a5053b21336f66f4977c2a324ee37b
Vincent Nys <vincentnys@gmail.com>
2020-07-26 09:41:24 +0200

    Moet de templates nog doen, maar dit geeft de structuur.

--- xchk_git_content/contentviews.py
+++ xchk_git_content/contentviews.py
@@ -23,3 +23,63 @@ class GitStagesView(ContentView):
     uid = 'git_stages_1'
     template = 'xchk_git_content/git_stages.html'
     strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+
+class GitStageChangesView(ContentView):
+
+    uid = 'git_stage_changes_1'
+    template = 'xchk_git_content/git_stage_changes.html'
+    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+
+class GitRemotesConceptView(ContentView):
+
+    uid = 'git_remotes_concept_1'
+    template = 'xchk_git_content/git_remotes_concept.html'
+    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+
+class GitIgnoreConceptView(ContentView):
+
+    uid = 'gitignore_concept_1'
+    template = 'xchk_git_content/gitignore_concept.html'
+    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+
+class GitStatusView(ContentView):
+
+    uid = 'git_status_1'
+    template = 'xchk_git_content/git_status.html'
+    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+
+class GitPushView(ContentView):
+
+    uid = 'git_push_1'
+    template = 'xchk_git_content/git_push.html'
+    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+
+class GitAddRemoteView(ContentView):
+
+    uid = 'git_add_remote_1'
+    template = 'xchk_git_content/git_add_remote.html'
+    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+
+class GitStatusAndGitIgnoreView(ContentView):
+
+    uid = 'git_status_and_gitignore_1'
+    template = 'xchk_git_content/git_status_and_gitignore.html'
+    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+    
+class GitCloneView(ContentView):
+
+    uid = 'git_clone_1'
+    template = 'xchk_git_content/git_clone.html'
+    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+
+class GitPullBasicsView(ContentView):
+
+    uid = 'git_pull_basics_1'
+    template = 'xchk_git_content/git_pull.html'
+    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
+    
+class GitLogView(ContentView):
+
+    uid = 'git_log_1'
+    template = 'xchk_git_content/git_log.html'
+    strat = Strategy(refusing_check=TrueCheck(),accepting_check=Negation(TrueCheck()))
--- xchk_git_content/course.py
+++ xchk_git_content/course.py
@@ -2,4 +2,18 @@ from xchk import contentviews as basecv
 from xchk.courses import Course
 from .contentviews import *
 
-course = Course('git','Git',[(GitStagesView,[GitInitView]),(GitInitView,[WhatIsGitView]),(WhatIsGitView,[basecv.ImpossibleNodeView])],"git@github.com:v-nys/git-modeloplossingen.git")
+course = Course('git',
+                'Git',
+                [(GitIgnoreConceptView,[GitStageChangesView]),
+                 (GitStatusView,[GitStageChangesView]),
+                 (GitPushView,[GitStageChangesView,GitRemotesConceptView]),
+                 (GitAddRemoteView,[GitRemotesConceptView]),
+                 (GitStatusAndGitIgnoreView,[GitStatusView,GitIgnoreConceptView]),
+                 (GitPullBasicsView,[GitPushView]),
+                 (GitLogView,[GitStageChangesView]),
+                 (GitStageChangesView,[GitStagesView]),
+                 (GitRemotesConceptView,[GitStagesView]),
+                 (GitStagesView,[GitInitView]),
+                 (GitInitView,[WhatIsGitView]),
+                 (WhatIsGitView,[basecv.ImpossibleNodeView])],
+                "git@github.com:v-nys/git-modeloplossingen.git")
