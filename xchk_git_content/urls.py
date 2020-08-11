38e2bfaba0f9d2a2b45df7afeb602eef42aeed90
Vincent Nys <vincentnys@gmail.com>
2020-08-11 10:34:02 +0200

    Fix typo

--- xchk_git_content/urls.py
+++ xchk_git_content/urls.py
@@ -8,4 +8,4 @@ from xchk_core.contentviews import ContentView
 def is_content_view(e):
     return inspect.isclass(e) and issubclass(e,ContentView)
 
-urlpatterns = [path(cv.uid, cv.as_view(), name=f'{cv.uid}_view') for cv in inspect.getmember(sys.modules['xchk_git_content.contentviews'],inspect.isclass)]
+urlpatterns = [path(cv.uid, cv.as_view(), name=f'{cv.uid}_view') for cv in inspect.getmembers(sys.modules['xchk_git_content.contentviews'],inspect.isclass)]
