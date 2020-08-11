27296d25a85d45a6b0828b7d5222edd058920915
Vincent Nys <vincentnys@gmail.com>
2020-08-11 10:39:23 +0200

    Had to update predicate

--- xchk_git_content/urls.py
+++ xchk_git_content/urls.py
@@ -9,4 +9,4 @@ from xchk_core.contentviews import ContentView
 def is_content_view(e):
     return inspect.isclass(e) and issubclass(e,ContentView)
 
-urlpatterns = [path(cv[1].uid, cv[1].as_view(), name=f'{cv[1].uid}_view') for cv in inspect.getmembers(sys.modules['xchk_git_content.contentviews'],inspect.isclass)]
+urlpatterns = [path(cv[1].uid, cv[1].as_view(), name=f'{cv[1].uid}_view') for cv in inspect.getmembers(sys.modules['xchk_git_content.contentviews'],is_content_view)]
